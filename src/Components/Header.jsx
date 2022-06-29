0829d64d591a6b1521d4a2d61e63b02050846da4
rahulsingh522003 <85445665+rahulsingh522003@users.noreply.github.com>
2022-06-29 19:11:17 +0530

    phone name change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -154,7 +154,7 @@ function Header() {
 
                 <Link to={'/'} className='flex items-center gap-2'>
                     <img src={Logo} className="w-8 object-cover" alt="logo" />
-                    <p className="text-headingColor text-xl font-bold">City</p>
+                    <p className="text-headingColor text-xl font-bold">Door2Door</p>
                 </Link>
 
                 <div className='relative'>
